8486ae55bb60159c150fef07ec0d1ddc7010f215
Hadar Naiman <hadarni@gmail.com>
2023-02-27 00:01:54 +0200

    dev cont

--- Routes/file.route.js
+++ Routes/file.route.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const router = express.Router();
 const fileBL = require("../BL/file.service")
-
+const multer = require("multer");
+const upload = multer({ dest: "myDrive/" })
 
 router.get("/get", async (req, res) => {
     console.log(req.query)
@@ -13,4 +14,17 @@ router.get("/get", async (req, res) => {
         console.log(error)
     }
 })
+
+router.post("/new", upload.any('file'), async (req, res) => {
+    try {
+        await fileBL.uploadFile(req.files, req.query.q);
+        res.send("success")
+    }
+    catch (error) {
+        console.log(error)
+    }
+})
+
 module.exports = router;
+
+//
